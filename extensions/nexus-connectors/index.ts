10636f6346806d4f21e3704881088b8662c435c1
The-Nexus-system <abrahamson.nicholas@gmail.com>
2026-09-19 15:35:02 -0400

    Wire developer platform tools into Nexus connectors

--- extensions/nexus-connectors/index.ts
+++ extensions/nexus-connectors/index.ts
@@ -1,5 +1,6 @@
 import { definePluginEntry } from "openclaw/plugin-sdk/plugin-entry";
 import { registerCreativeTools } from "./creative-tools.js";
+import { registerDeveloperTools } from "./developer-tools.js";
 import { registerGeneralTools } from "./general-tools.js";
 import { registerGoogleTools } from "./google-tools.js";
 import { registerGoogleOAuth } from "./google-oauth.js";
@@ -24,6 +25,7 @@ export default definePluginEntry({
     registerStatusTools(api);
     registerGeneralTools(api);
     registerCreativeTools(api);
+    registerDeveloperTools(api);
     registerMicrosoftTools(api);
     registerGoogleTools(api);
     registerGoogleOAuth(api);
